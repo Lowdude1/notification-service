129d34c56977e8957f999f7d3237be8a96313a29
Lowdude1 <sanitaronlain@gmail.com>
2026-05-13 23:16:21 +0300

    Небольшая корректировка + очистка данных при каждом запуске приложения

--- src/main/java/com/example/Notification_Service/config/RedisConfig.java
+++ src/main/java/com/example/Notification_Service/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.example.Notification_Service.config;
 import com.example.Notification_Service.Service.RedisNotificationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
